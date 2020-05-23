7f6d8a1c19e000ebd88d15a7105a17e9227ac528
Joe <dombinorbert1@gmail.com>
2020-05-23 13:24:49 +0300

    Modified controllers to allow merge

--- ISS Project/src/main/java/ubb/project/iss/controller/AttendanceController.java
+++ ISS Project/src/main/java/ubb/project/iss/controller/AttendanceController.java
@@ -14,21 +14,40 @@ public class AttendanceController {
     @Autowired
     private AttendanceService attendanceService;
 
-    @RequestMapping(value = "/attendences", method = RequestMethod.GET)
+    @RequestMapping(value = "/attendances", method = RequestMethod.GET)
     List<Attendance> getConferences()
     {
         return attendanceService.getAll();
     }
 
-    @RequestMapping(value = "/attendences", method = RequestMethod.POST)
+    @RequestMapping(value = "/attendances", method = RequestMethod.POST)
     Attendance save(@RequestBody Attendance attendance)
     {
         return attendanceService.save(attendance);
     }
 
-    @RequestMapping(value = "/attendencesx/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/attendances/{id}", method = RequestMethod.GET)
     Attendance getById(@PathVariable Long id)
     {
         return attendanceService.getById(id);
+    }
+
+    @RequestMapping(value = "attendances/{id}", method = RequestMethod.DELETE)
+    void cancelSignUp(@PathVariable Long id)
+    {
+        Attendance attendance = attendanceService.getById(id);
+        attendanceService.removeAttendance(attendance);
+    }
+
+    @RequestMapping(value = "/users_attending/{id}", method = RequestMethod.GET)
+    List<UserTable> getUsersAttendingConference(@PathVariable Long id)
+    {
+        return attendanceService.getAllUsersAttendingConference(id);
+    }
+
+    @RequestMapping(value = "/conferences_attended/{id}", method = RequestMethod.GET)
+    List<Conference> getConferencesAttendedByUser(@PathVariable Long id)
+    {
+        return attendanceService.getAllConferencesAttendedByUser(id);
     }
 }
