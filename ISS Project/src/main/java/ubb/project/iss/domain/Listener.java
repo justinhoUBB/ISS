fa74e1a394ef775f58c7e15277072977a5142691
cipri <cipriangabor99@yahoo.com>
2020-05-28 14:49:38 +0300

    part 5, sections on conference and listeners

---
+++ ISS Project/src/main/java/ubb/project/iss/domain/Listener.java
@@ -0,0 +1,18 @@
+package ubb.project.iss.domain;
+import lombok.*;
+
+import javax.persistence.Entity;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@Builder
+public class Listener extends BaseEntity<Long> {
+    private long member_id;
+    private long conference_id;
+    private String section_name;
+}
+
